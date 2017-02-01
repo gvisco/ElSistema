a437871b8dbcc74db73773fb8671f6ac6d9e7c08
gvisco <giuliovisco@gmail.com>
2017-02-01 20:54:35 +0100

    cleanup

--- sketch.js
+++ sketch.js
@@ -1,8 +1,6 @@
 var targets = [];
 var population;
 
-//var tree;
-
 function setup() {
     //noLoop();
     frameRate(50);
@@ -10,28 +8,15 @@ function setup() {
     createCanvas(600, 600);
     
     population = new Population(100);
-
-    //r1 = new Rule("X", "]++[-[++[-[]++[-[++[-[]221F--F--F--");
-    //r2 = new Rule("Y", "");
-    
-    //r1 = new Rule("1", "FFF[-F[++F]F]+FF+F");
-    //r2 = new Rule("2", "");
-
-    //r1 = new Rule("F", "Ff[f+[-F--f]>F]<[f]");
-    
-    //r1 = new Rule("F", "F[-F][+F1F-F++F]");
-    //r2 = new Rule("1", "F[-F+F]");
-    
-    //lsystem = new LSystem("1", [r1, r2]);
-    //tree = new Tree(lsystem, 4, 100);
 }
 
 function draw() {
     background(51, 51, 51);
+    if (targets.length == 0) {
+        return;
+    }
 
     population.next();
-
-    //tree.draw();
 
     stroke(255);
     strokeWeight(2);
@@ -39,10 +24,6 @@ function draw() {
         p = targets[i];
         ellipse(p.x, p.y, 5);
     }
-
-
-    //createP("Size: " + tree.size());
-    //createP(tree.sentence);
 }
 
 function mouseClicked() {
